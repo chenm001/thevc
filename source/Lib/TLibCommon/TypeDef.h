d1e91cdd0ac3549a9e44da86a4755a694ead8503
bossen <bossen@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2011-01-25 01:42:56 +0000

    Enable fix for issue #119 (deblocking filter)
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-1.0-dev@457 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -142,7 +142,7 @@
 #define BUGFIX102 1 // Do not code terminating bit when using LCEC
 #define BUGFIX110 1 // Fix an issue related to interpolation filters and cost calculation
 #define BUGFIX118 1 // Fixes an issue related to ALF and all-0 filters
-#define BUGFIX119 0 // Fixes an issue with the deblocking filter for chroma
+#define BUGFIX119 1 // Fixes an issue with the deblocking filter for chroma
 
 ////////////////////////////////
 // MICROSOFT&USTC defines section start
