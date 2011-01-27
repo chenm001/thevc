d25dc4d657b9096a553b87175b5bcf2915c96367
bossen <bossen@ce97f6c2-8f86-0410-b44d-af84b13ec34f>
2011-01-27 07:37:58 +0000

    Change TPE settings to 2-bit expansion (D440)
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-1.0-dev@481 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/CommonDef.h
+++ source/Lib/TLibCommon/CommonDef.h
@@ -172,10 +172,10 @@
 #define EARLY_SKIP_THRES            1.50        ///< if RD < thres*avg[BestSkipRD]
 
 
-const int g_iShift8x8    = 4;
-const int g_iShift16x16  = 4;
-const int g_iShift32x32  = 4;
-const int g_iShift64x64  = 4;
+const int g_iShift8x8    = 2;
+const int g_iShift16x16  = 2;
+const int g_iShift32x32  = 2;
+const int g_iShift64x64  = 2;
 
 /* End of Rounding control */
 
