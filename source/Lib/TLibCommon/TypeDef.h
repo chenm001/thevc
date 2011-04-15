b9d99d65c24f1972f6e8a51a3bacf00db7001e4b
bossen <bossen>
2011-04-15 04:03:35 +0000

    Merged r753 from HM-2.2-dev-sony
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-2.2-dev@793 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -315,7 +315,7 @@
 /////////////////////////////////
 
 #define MVD_CTX            1           // JCTVC-E324: Modified context selection for MVD
-#define PARALLEL_DEBLK_DECISION      0 // JCTC-E224: Parallel decisions
+#define PARALLEL_DEBLK_DECISION      1 // JCTC-E224: Parallel decisions
 #define PARALLEL_MERGED_DEBLK        1 // JCTC-E224, JCTVC-E181: Parallel decisions + Parallel filtering
 #define REFERENCE_SAMPLE_PADDING                1   // JCTVC-E488 (Ericsson, HiSilicon, NEC, Panasonic): padding of unavailable reference samples for intra prediction
 
