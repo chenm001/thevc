f02659cab96d47b7adbd8a639c77ebe3ad7b5a67
bossen <bossen>
2011-05-30 04:39:58 +0000

    Cleanup of ContextModel3DBuffer class
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-3.1-dev@975 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/ContextModel3DBuffer.h
+++ source/Lib/TLibCommon/ContextModel3DBuffer.h
@@ -35,12 +35,8 @@
     \brief    context model 3D buffer class (header)
 */
 
-#ifndef __CONTEXT_MODEL_3DBUFFER__
-#define __CONTEXT_MODEL_3DBUFFER__
-
-#if _MSC_VER > 1000
-#pragma once
-#endif // _MSC_VER > 1000
+#ifndef _HM_CONTEXT_MODEL_3DBUFFER_H_
+#define _HM_CONTEXT_MODEL_3DBUFFER_H_
 
 #include <stdio.h>
 #include <assert.h>
@@ -57,10 +53,10 @@
 class ContextModel3DBuffer
 {
 protected:
-  ContextModel* m_pcContextModel;                                         ///< array of context models
-  const UInt    m_uiSizeX;                                                ///< X size of 3D buffer
-  const UInt    m_uiSizeY;                                                ///< Y size of 3D buffer
-  const UInt    m_uiSizeZ;                                                ///< Z size of 3D buffer
+  ContextModel* m_contextModel; ///< array of context models
+  const UInt    m_sizeX;        ///< X size of 3D buffer
+  const UInt    m_sizeXY;       ///< X times Y size of 3D buffer
+  const UInt    m_sizeXYZ;      ///< total size of 3D buffer
   
 public:
   ContextModel3DBuffer  ( UInt uiSizeZ, UInt uiSizeY, UInt uiSizeX );
@@ -69,21 +65,28 @@ public:
   // access functions
   ContextModel& get( UInt uiZ, UInt uiY, UInt uiX )
   {
-    return  m_pcContextModel[ ( uiZ * m_uiSizeY + uiY ) * m_uiSizeX + uiX ];
+    return  m_contextModel[ uiZ * m_sizeXY + uiY * m_sizeX + uiX ];
   }
   ContextModel* get( UInt uiZ, UInt uiY )
   {
-    return &m_pcContextModel[ ( uiZ * m_uiSizeY + uiY ) * m_uiSizeX       ];
+    return &m_contextModel[ uiZ * m_sizeXY + uiY * m_sizeX ];
   }
   ContextModel* get( UInt uiZ )
   {
-    return &m_pcContextModel[ ( uiZ * m_uiSizeY       ) * m_uiSizeX       ];
+    return &m_contextModel[ uiZ * m_sizeXY ];
   }
   
   // initialization & copy functions
   Void initBuffer( SliceType eSliceType, Int iQp, Short* psCtxModel );          ///< initialize 3D buffer by slice type & QP
-  Void copyFrom  ( ContextModel3DBuffer* pSrc                       );          ///< copy from given 3D buffer
+  
+  /** copy from another buffer
+   * \param pSrc buffer to copy from
+   */
+  Void copyFrom( ContextModel3DBuffer* src )
+  {
+    assert( m_sizeXYZ == src->m_sizeXYZ );
+    ::memcpy( m_contextModel, src->m_contextModel, sizeof(ContextModel) * m_sizeXYZ );
+  }
 };
 
-#endif
-
+#endif // _HM_CONTEXT_MODEL_3DBUFFER_H_
