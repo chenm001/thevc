3391bbd36e041f39a5a1f40d290ef021f9e461ed
Tung Nguyen <tung.nguyen@hhi.fraunhofer.de>
2011-07-28 13:03:10 +0000

    From: Tung Nguyen <tung.nguyen@hhi.fraunhofer.de> Changes: - remove ENABLE_CONTEXT_REDUCTIONS macro          - assign document number for each macro
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-3.4-dev-cabac@1154 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -42,29 +42,15 @@
 // BoG Context Reduction
 ////////////////////////////
 
-#define ENABLE_CONTEXT_REDUCTIONS         1
-
-#if ENABLE_CONTEXT_REDUCTIONS
-#define MODIFIED_MVD_CODING               1           ///< modified binarization, coding and bin reordering of MVD
-#define MODIFIED_LAST_CODING              1           ///< modified binarization and coding of last significant position
-#define DNB_MERGE_FLAG                    1           ///< disable of neighbour evaluation for merge_flag
-#define DNB_INTRA_CHR_PRED_MODE           1           ///< disable of neighbour evaluation for intra_chroma_pred_mode
-#define DNB_REF_FRAME_IDX                 1           ///< disable of neighbour evaluation for ref_frame_idx
-#define DNB_ALF_CTRL_FLAG                 1           ///< disable of neighbour evaluation for alf_ctrl_flag
-#define DNB_INTER_PRED_MODE               1           ///< disable of neighbour evaluation for inter_pred_flag (use depth for that)
-#define DNB_CHROMA_CBF_FLAGS              1           ///< disable of neighbour evaluation for chroma cbf (use depth for that)
-#define DNB_LUMA_CBF_FLAGS                1           ///< disable of neighbour evaluation for luma cbf
-#else
-#define MODIFIED_MVD_CODING               0           ///< modified binarization, coding and bin reordering of MVD
-#define MODIFIED_LAST_CODING              0           ///< modified binarization and coding of last significant position
-#define DNB_MERGE_FLAG                    0           ///< disable of neighbour evaluation for merge_flag
-#define DNB_INTRA_CHR_PRED_MODE           0           ///< disable of neighbour evaluation for intra_chroma_pred_mode
-#define DNB_REF_FRAME_IDX                 0           ///< disable of neighbour evaluation for ref_frame_idx
-#define DNB_ALF_CTRL_FLAG                 0           ///< disable of neighbour evaluation for alf_ctrl_flag
-#define DNB_INTER_PRED_MODE               0           ///< disable of neighbour evaluation for inter_pred_flag
-#define DNB_CHROMA_CBF_FLAGS              0           ///< disable of neighbour evaluation for chroma cbf (use depth for that)
-#define DNB_LUMA_CBF_FLAGS                0           ///< disable of neighbour evaluation for luma cbf
-#endif
+#define MODIFIED_MVD_CODING               1           ///< F455 / F423:        modified binarization, coding and bin reordering of MVD
+#define MODIFIED_LAST_CODING              1           ///< F375:               modified binarization and coding of last significant position
+#define DNB_MERGE_FLAG                    1           ///< F429:               disable of neighbour evaluation for merge_flag
+#define DNB_INTRA_CHR_PRED_MODE           1           ///< F497:               disable of neighbour evaluation for intra_chroma_pred_mode
+#define DNB_REF_FRAME_IDX                 1           ///< F429 / F497 / F606: disable of neighbour evaluation for ref_frame_idx
+#define DNB_ALF_CTRL_FLAG                 1           ///< F429:               disable of neighbour evaluation for alf_ctrl_flag
+#define DNB_INTER_PRED_MODE               1           ///< F606:               disable of neighbour evaluation for inter_pred_flag (use depth for that)
+#define DNB_CHROMA_CBF_FLAGS              1           ///< F606:               disable of neighbour evaluation for chroma cbf (use depth for that)
+#define DNB_LUMA_CBF_FLAGS                1           ///< F429:               disable of neighbour evaluation for luma cbf
 
 ////////////////////////////
 // JCT-VC E start
