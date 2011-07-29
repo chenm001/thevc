d519dbe93c7552eb62cfed44d1812f16e629ab3d
bossen <bossen>
2011-07-29 21:18:12 +0000

    Add reference to document
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-3.4-dev-intra@1190 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -203,7 +203,7 @@
 #define MN_DC_PRED_FILTER            1       ///< JCTVC-E069: DC prediction samples filtering
 #define UNIFICATION_OF_AVAILABILITY  1       ///< JCTVC-F178: unification of sample availability for DC
 #if MN_DC_PRED_FILTER
-#define MN_DC_PRED_FILTER_UNIFIED    1       // using unified filter for all block size
+#define MN_DC_PRED_FILTER_UNIFIED    1       ///< JCTVC-F252 using unified filter for all block size
 #endif
 
 #define MVD_CTX            1           // JCTVC-E324: Modified context selection for MVD
