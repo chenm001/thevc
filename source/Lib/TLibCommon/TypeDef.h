9ff6fde274b44a9559a59c7ad7b70831fc8a5f24
bossen <bossen>
2012-02-09 17:47:47 +0000

    Remove CBF_CODING_SKIP_COND_FIX macro
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-5.1-dev-cleanup@1902 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -49,7 +49,6 @@
 
 #define WEIGHT_PRED_IMP           1 ///< high-precision offset for weighted bipred G065
 #define PLANAR_IS_DEFAULT         1 ///< default to planar if neighbor not available G119
-#define CBF_CODING_SKIP_COND_FIX  1 ///< G444: fixing the condition of skipping cbf_luma coding
 #define VER_HOR_FILTER            1 ///< F172: intra ver/hor prediction filter
 #define WP_IMPROVED_SYNTAX        1 ///< improved weighted prediction syntax to remove redundancy G441
 
