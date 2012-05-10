845703498940e80f4584ea6f2e43b6a6a77c0101
panasonic <panasonic>
2012-05-10 06:22:17 +0000

    I0134/I0314: fill up the empty list of AMVP/Merge with the macro FILLUP_EMPTYLIST_AMVP_MERGE
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-6.3-dev@2330 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/TComDataCU.cpp
+++ source/Lib/TLibCommon/TComDataCU.cpp
@@ -2913,7 +2913,13 @@ Void TComDataCU::getInterMergeCandidates( UInt uiAbsPartIdx, UInt uiPUIdx, UInt 
   }
 
   Int iNumRefIdx = (getSlice()->isInterB()) ? min(m_pcSlice->getNumRefIdx(REF_PIC_LIST_0), m_pcSlice->getNumRefIdx(REF_PIC_LIST_1)) : m_pcSlice->getNumRefIdx(REF_PIC_LIST_0);
+#if FILLUP_EMPTYLIST_AMVP_MERGE
+  Int r = 0;
+  Int refcnt = 0;
+  while (uiArrayAddr < MRG_MAX_NUM_CANDS)
+#else
   for (int r=0; r<iNumRefIdx && uiArrayAddr!=MRG_MAX_NUM_CANDS; r++)
+#endif
   {
     abCandIsInter[uiArrayAddr] = true;
     puhInterDirNeighbours[uiArrayAddr] = 1;
@@ -2925,6 +2931,17 @@ Void TComDataCU::getInterMergeCandidates( UInt uiAbsPartIdx, UInt uiPUIdx, UInt 
       pcMvFieldNeighbours[(uiArrayAddr << 1) + 1].setMvField(TComMv(0, 0), r);
     }
     uiArrayAddr++;
+#if FILLUP_EMPTYLIST_AMVP_MERGE
+    if ( refcnt == iNumRefIdx - 1 )
+    {
+      r = 0;
+    }
+    else
+    {
+      ++r;
+      ++refcnt;
+    }
+#endif
   }
   if (uiArrayAddr > MRG_MAX_NUM_CANDS_SIGNALED)
   {
@@ -3319,7 +3336,11 @@ Void TComDataCU::fillMvpCand ( UInt uiPartIdx, UInt uiPartAddr, RefPicList eRefP
   {
     pInfo->iN = AMVP_MAX_NUM_CANDS;
   }
+#if FILLUP_EMPTYLIST_AMVP_MERGE
+  while (pInfo->iN < AMVP_MAX_NUM_CANDS)
+#else
   else if (pInfo->iN < AMVP_MAX_NUM_CANDS)
+#endif
   {
       pInfo->m_acMvCand[pInfo->iN].set(0,0);
       pInfo->iN++;
