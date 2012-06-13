15528bf2aa76a7c604f52b8f1b97d5858eabfbd7
qualcomm <qualcomm>
2012-06-13 03:02:08 +0000

    JCTVC-I0600: semantics of temporal_id changed; temporal_id of SEI NALUnit shall not be less than the temporal_id of containing access unit, and temporal id of SPS NALUnit shall be 0.(MACRO: TEMPORAL_ID_RESTRICTION); from Adarsh K. Ramasubramonian <aramasub@qualcomm.com>
    git-svn-id: svn://hevc.kw.bbc.co.uk/svn/jctvc-tmuc/branches/HM-7.0-dev@2450 ce97f6c2-8f86-0410-b44d-af84b13ec34f

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -40,6 +40,7 @@
 
 //! \ingroup TLibCommon
 //! \{
+#define  TEMPORAL_ID_RESTRICTION         1  ///< I0600: Semantics of temporal_id changed
 #define PREVREFPIC_DEFN                    1   ///< I0345: prevRefPic defined as previous reference picture that is at same or lower 
                                             ///< temporal layer.
 #define CODE_POCLSBLT_FIXEDLEN      1       ///< I0342: code the LSB bits of LTRPs as u(v)
